5c9d3e3ddee922cedda8b19790030868197c53b5
utkar-sh-ukla <utkarshshukla707@gmail.com>
2023-03-13 23:35:03 +0530

    httpServer using Java

---
+++ httpServer/src/Main.java
@@ -0,0 +1,18 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.Date;
+
+public class Main {
+  public static void main(String[] args) throws IOException{
+    final ServerSocket server = new ServerSocket(8080);
+    System.out.println("Listening for connection on port 8080 .....");
+    while (true) {
+      try(Socket socket = server.accept()) {
+        Date today = new Date();
+        String httpResponse = "HTTP/1.1 200 OK\r\n\r\n" + today;
+        socket.getOutputStream().write(httpResponse.getBytes("UTF-8"));
+      }
+    }
+  }
+}
